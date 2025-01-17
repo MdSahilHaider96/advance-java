eacf072ccbe1598fab5c7add555ecaa28c17a916
Sahil <mdsahilhaider96@gmail.com>
2025-01-17 07:29:30 +0530

    checking

---
+++ first-web-project/src/org/first/model/Users.java
@@ -0,0 +1,12 @@
+package org.first.model;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+//Comperator comperable
+public class Users {
+
+    private List<Users> usersList = new ArrayList<>();
+    private Users users;
+}
